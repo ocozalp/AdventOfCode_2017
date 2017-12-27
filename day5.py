636a80704bc02c8d4fc146051f69f1b7b817cdea
ocozalp <ozalp.orhan@gmail.com>
2017-12-27 12:16:44 -0500

    Day 5

---
+++ day5.py
@@ -0,0 +1,18 @@
+def solve(numbers):
+    n = len(numbers)
+    print 'n', n
+    c = 0
+    steps = 0
+    while 0 <= c < n:
+        steps += 1
+        next_step = numbers[c]
+        numbers[c] += 1
+        c += next_step
+
+    return steps
+
+
+with open('day5.txt', 'r') as f:
+    numbers = map(int, map(lambda l: l[:-1], f.readlines()))
+    res = solve(numbers)
+    print res
