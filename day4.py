516b6395ef7f3f1e4ab18f1c65af5025b5bea629
ocozalp <ozalp.orhan@gmail.com>
2017-12-28 16:33:21 -0500

    Refactor Day 4

--- day4.py
+++ day4.py
@@ -1,16 +1,40 @@
-def is_valid(passphrase):
+def all_unique(passphrase):
     tokens = passphrase.split()
     return len(tokens) == len(set(tokens))
 
-def solve(lines):
+
+def no_anagrams(passphrase):
+    tokens = map(sort_word, passphrase.split())
+    return len(tokens) == len(set(tokens))
+
+
+def sort_word(word):
+    return ''.join(sorted(word))
+
+
+def solve(lines, validator):
     result = 0
     for line in lines:
-        if is_valid(line):
+        if validator(line):
             result += 1
     return result
 
-with open('day4.txt', 'r') as f:
-    lines = map(lambda line: line[:-1], f.readlines())
-    res = solve(lines)
 
-    print res
+def part1(lines):
+    return solve(lines, all_unique)
+
+
+def part2(lines):
+    return solve(lines, no_anagrams)
+
+
+def main():
+    with open('input/day4.txt', 'r') as f:
+        lines = map(lambda line: line[:-1], f.readlines())
+
+    print 'Part 1: ', part1(lines)
+    print 'Part 2: ', part2(lines)
+
+
+if __name__ == '__main__':
+    main()
