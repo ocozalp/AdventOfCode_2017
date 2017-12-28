028f90de1506b37d79fc2dff2aaabe86a2ee48c7
ocozalp <ozalp.orhan@gmail.com>
2017-12-28 16:27:37 -0500

    Refactor Day 2

--- day2.py
+++ day2.py
@@ -1,9 +1,21 @@
-result = 0
-with open('day2.txt') as f:
-    lines = f.readlines()
+def part1(data):
+    return sum([max(tokens) - min(tokens) for tokens in data])
 
-    for line in lines:
-        tokens = map(int, str(line[:-1]).split('\t'))
-        result += max(tokens) - min(tokens)
 
-print result
+def part2(data):
+    from itertools import permutations
+
+    return sum([sum([num1 / num2 for num1, num2 in permutations(tokens, 2) if num1 > num2 and num1 % num2 == 0])
+                for tokens in data])
+
+
+def main():
+    with open('input/day2.txt') as f:
+        data = [map(int, str(line[:-1]).split('\t')) for line in f.readlines()]
+
+    print 'Part 1: ', part1(data)
+    print 'Part 2: ', part2(data)
+
+
+if __name__ == '__main__':
+    main()
