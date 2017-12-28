e386b0962903bf5aa31437af84ded9f196d12061
ocozalp <ozalp.orhan@gmail.com>
2017-12-28 16:16:47 -0500

    Refactor Day 1

--- day1.py
+++ day1.py
@@ -1,11 +1,29 @@
-captcha = raw_input()
+def solve(captcha, offset):
+    result = 0
+    n = len(captcha)
+    for i in xrange(len(captcha)):
+        next_index = (i + offset) % n
+        if captcha[i] == captcha[next_index]:
+            result += ord(captcha[i]) - ord('0')
 
-result = 0
-n = len(captcha)
+    return result
 
-for i in xrange(len(captcha)):
-    next_index = (i+1) % n
-    if captcha[i] == captcha[next_index]:
-        result += ord(captcha[i]) - ord('0')
 
-print result
+def part1(captcha):
+    return solve(captcha, 1)
+
+
+def part2(captcha):
+    return solve(captcha, len(captcha)//2)
+
+
+def main():
+    with open('input/day1.txt', 'r') as f:
+        captcha = f.readline()[:-1]
+
+    print 'Part 1: ', part1(captcha)
+    print 'Part 2: ', part2(captcha)
+
+
+if __name__ == '__main__':
+    main()
