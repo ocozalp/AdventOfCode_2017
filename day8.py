aa19d7cc539816eaa8f2c49323e99fa2fdf45917
ocozalp <ozalp.orhan@gmail.com>
2017-12-28 18:14:35 -0500

    Day 8 - Part 2

--- day8.py
+++ day8.py
@@ -24,6 +24,7 @@ def eval_condition(registers, r, op, arg):
 
     raise Exception('Unexpected')
 
+
 def evaluate(registers, r1, op1, arg1, r2, op2, arg2):
     if not eval_condition(registers, r2, op2, arg2):
         return
@@ -40,25 +41,29 @@ def evaluate(registers, r1, op1, arg1, r2, op2, arg2):
     else:
         raise Exception('Unexpected operator')
 
-    registers[r1] = v
+    return v
 
 
 def solve(lines):
     registers = dict()
+    max_reg_val = -10000000000000000
+
     for l in lines:
-        evaluate(registers, l[0], l[1], int(l[2]), l[4], l[5], int(l[6]))
+        v = evaluate(registers, l[0], l[1], int(l[2]), l[4], l[5], int(l[6]))
+        if v is not None:
+            registers[l[0]] = v
+            max_reg_val = max(max_reg_val, v)
+
+    return max(registers.values()), max_reg_val
 
 
-    mval = -10000000000000000
-    res = -1
-    for k in registers:
-        if registers[k] > mval:
-            mval = registers[k]
-            res = k
+def main():
+    with open('input/day8.txt', 'r') as f:
+        lines = map(lambda l: l[:-1].split(), f.readlines())
 
-    return mval
+    part1, part2 = solve(lines)
+    print 'Part 1: ', part1
+    print 'Part 2: ', part2
 
-with open('day8.txt', 'r') as f:
-    lines = map(lambda l: l[:-1].split(), f.readlines())
-    res = solve(lines)
-    print res
+if __name__ == '__main__':
+    main()
